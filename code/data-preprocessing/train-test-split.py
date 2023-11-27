41d0b0e958e210a1cc4d38ab9c9edec14c36b2d7
MUSKAAN CHOPRA <61360636+muskaan712@users.noreply.github.com>
2023-11-27 22:47:16 +0100

    train-test-split

--- code/data-preprocessing/train-test-split.py
+++ code/data-preprocessing/train-test-split.py
@@ -1,43 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-# In[2]:
-
-
 # Load the dataset
 df = pd.read_csv('relish.tsv', sep='\t')
-
-
-# In[3]:
-
-
 df.columns=['PID1', 'PID2','Value']
-
-
-# In[6]:
-
 
 from sklearn.model_selection import train_test_split 
 train, test = train_test_split(df, test_size=0.20, stratify=df['Value'])
 
-
-# In[7]:
-
-
 # Save the training and test sets to .tsv files
 train.to_csv('RELISH_Training_Dataset.tsv', sep='\t', index=False)
 test.to_csv('RELISH_Test_Dataset.tsv', sep='\t', index=False)
-
-
-# In[8]:
-
 
 # Load the dataset
 dftr = pd.read_csv('RELISH_Training_Dataset.tsv', sep='\t')
